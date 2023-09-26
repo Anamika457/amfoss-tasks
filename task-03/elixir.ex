ebcdb7f6e52d2f1d5fc0a47221ce2d3bfd9d75cc
Anamika457 <143307770+Anamika457@users.noreply.github.com>
2023-09-26 13:38:00 +0530

    Update elixir.ex

--- task-03/elixir.ex
+++ task-03/elixir.ex
@@ -1,29 +1,21 @@
-defmodule PrimeNumbers do
-  def is_prime(num) when num <= 1, do: false
-  def is_prime(2), do: true
-  def is_prime(num) when rem(num, 2) == 0, do: false
-  def is_prime(num) do
-    is_prime(num, 3)
+defmodule Prime do
+  def nth_prime(n), do: prime(n, 2)
+def is_prime(x), do: (2..x |> Enum.filter(fn a -> rem(x, a) == 0 end) |> length()) == 1
+def prime(n, n), do: []
+def prime(n, acc) do
+  case is_prime(acc) do
+    true -> [acc | prime(n, acc + 1)]
+    false -> prime(n, acc + 1)
   end
-
-  defp is_prime(num, divisor) when divisor * divisor > num, do: true
-  defp is_prime(num, divisor) when rem(num, divisor) == 0, do: false
-  defp is_prime(num, divisor) do
-    is_prime(num, divisor + 2)
+end 
+  
+  def start do
+    {n, _} = IO.gets("Enter a number: ") |> Integer.parse
+ 
+    i = nth_prime(n)
+    IO.inspect i
   end
-
-  def run do
-    IO.puts("Enter a positive integer (n):")
-    case String.trim(IO.gets("")).to_integer() do
-      {:ok, n} when n >= 2 ->
-        IO.puts("Prime numbers up to #{n} are:")
-        Enum.each(2..n, fn x -> if is_prime(x), do: IO.write("#{x} ") end)
-        IO.puts("")
-
-      _ ->
-        IO.puts("Invalid input. Please enter a valid positive integer.")
-    end
-  end
+  
 end
 
-PrimeNumbers.run()
+Prime.start()
